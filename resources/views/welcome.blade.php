d0b8d9a597fcd0de05f2806b5d91a0c3e3402ae0
Yuchiima <134304602+Yuchiima@users.noreply.github.com>
2023-05-25 07:32:53 +0400

    FRONTEND - V1.0.0.0 - Integration de la meta description.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Un service qui permet de vérifier la disponibilité d'un nom de domaine.">
     <title>Avadomain - Service disponibilité nom de domaine.</title>
 
     <link rel="stylesheet" href="{{ asset("css/style.css") }}">
